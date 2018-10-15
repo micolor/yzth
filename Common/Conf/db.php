c658bc343d63a45aea2d04b16dae81c3aec7cb7d
Ann <wangwenan@ibeifeng.com>
2018-10-15 17:46:12 +0800

    bug修改

--- Common/Conf/db.php
+++ Common/Conf/db.php
@@ -4,22 +4,13 @@
  */
 return array(
     'DB_TYPE' => 'mysql',     // 数据库类型
-    'DB_HOST' => 'localhost', // 服务器地址
-    'DB_NAME' => 'yzth',          // 数据库名
+    'DB_HOST' => '45.62.109.108', // 服务器地址
+    'DB_NAME' => 'yzsx',          // 数据库名
     'DB_USER' => 'root',      // 用户名
-    'DB_PWD' => '1qaz@wsx',          // 密码
+    'DB_PWD' => 'kzd337836629',          // 密码
     'DB_PORT' => '3306',// 端口
     'DB_PREFIX' => 'mc_',// 数据库表前缀
     //密钥
-    "AUTHCODE" => 'TOACUz462LIMvq79jB',
-    // 业务系统
-    'DB_BS' => array(
-        'DB_TYPE' => 'mysql',
-        'DB_HOST' => '121.43.108.228',
-        'DB_PORT' => '3306',
-        'DB_NAME' => 'jxgcdbtst',
-        'DB_USER' => 'jxgc',
-        'DB_PWD' => 'jxgc',
-    )
+    "AUTHCODE" => 'TOACUz462LIMvq79jA',
 );
 ?>
